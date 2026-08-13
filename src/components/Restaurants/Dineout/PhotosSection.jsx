cded2051261f85a18e29da0a2b493de2fa95594b
Aman Kumar Singh <125180773+amankumarsingh246810@users.noreply.github.com>
2026-08-13 16:48:59 +0530

    Update PhotosSection component layout

--- src/components/Restaurants/Dineout/PhotosSection.jsx
+++ src/components/Restaurants/Dineout/PhotosSection.jsx
@@ -1,6 +1,6 @@
 export default function PhotosSection ({restaurantData}) {
     return(
-        < div className="w-[70%]">    
+        < div className="w-full lg:w-[70%]">    
         <button className="text-md font-[500] mb-2 mt-1 border-1 bg-gray-200 rounded-3xl px-4 py-1">PHOTOS</button>    
         <div className=" flex flex-wrap gap-4 mt-4">
             {
